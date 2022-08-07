46d40dc59e01f4dc7ae9390ccc3c73f36d64744f
Martin Salinas <martin.salinas@gmail.com>
2022-08-07 14:45:56 -0300

    refactor: improve questionsService test

--- src/domain/questionsService.test.ts
+++ src/domain/questionsService.test.ts
@@ -2,79 +2,86 @@ import { getQuestions } from './questionsService';
 
 const URL = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function setupFetchStub(data: any) {
+  return function fetchStub() {
+    return Promise.resolve({
+      json: () => Promise.resolve(data),
+    } as Response);
+  };
+}
+
 describe('questions service', () => {
   test('getQuestions should return response.results', async () => {
-    const globalFetch = global.fetch;
-    const mockFetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            response_code: 0,
-            results: [{}],
-          }),
-      } as Response),
-    );
-    global.fetch = mockFetch;
+    const fakeData = {
+      response_code: 0,
+      results: [{}, {}, {}],
+    };
+
+    if (typeof global.fetch === 'function') {
+      jest.spyOn(global, 'fetch').mockImplementation(setupFetchStub(fakeData));
+    } else {
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(fakeData));
+    }
+
     const ourResponse = await getQuestions();
-    expect(ourResponse.length).toEqual(1);
-    global.fetch = globalFetch;
+    expect(ourResponse.length).toEqual(3);
+
+    (global.fetch as jest.MockedFunction<typeof global.fetch>).mockClear();
   });
   test('getQuestions should throw error with message if no results', async () => {
-    const globalFetch = global.fetch;
-    const mockFetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            response_code: 1,
-            results: [],
-          }),
-      } as Response),
-    );
-    global.fetch = mockFetch;
+    const fakeData = {
+      response_code: 1,
+      results: [],
+    };
+    if (typeof global.fetch === 'function') {
+      jest.spyOn(global, 'fetch').mockImplementation(setupFetchStub(fakeData));
+    } else {
+      global.fetch = jest.fn().mockImplementation(setupFetchStub(fakeData));
+    }
+
     const callGetQuestions = async () => {
       await getQuestions();
     };
     expect(callGetQuestions).rejects.toThrowError('No Results');
-    // expect ?
-    global.fetch = globalFetch;
+
+    (global.fetch as jest.MockedFunction<typeof global.fetch>).mockClear();
   });
   describe('getQuestions params', () => {
     test('without params should fetch default URL', async () => {
-      const globalFetch = global.fetch;
-      const mockFetch = jest.fn(() =>
-        Promise.resolve({
-          json: () =>
-            Promise.resolve({
-              response_code: 0,
-              results: [{}],
-            }),
-        } as Response),
-      );
+      const fakeData = {
+        response_code: 0,
+        results: [{}],
+      };
+      if (typeof global.fetch === 'function') {
+        jest.spyOn(global, 'fetch').mockImplementation(setupFetchStub(fakeData));
+      } else {
+        global.fetch = jest.fn().mockImplementation(setupFetchStub(fakeData));
+      }
 
-      global.fetch = mockFetch;
       await getQuestions();
-      expect(mockFetch).toHaveBeenCalledWith(URL);
-      global.fetch = globalFetch;
+      expect(global.fetch).toHaveBeenCalledWith(URL);
+      (global.fetch as jest.MockedFunction<typeof global.fetch>).mockClear();
     });
     test('with params should fetch custom URL', async () => {
-      const globalFetch = global.fetch;
-      const mockFetch = jest.fn(() =>
-        Promise.resolve({
-          json: () =>
-            Promise.resolve({
-              response_code: 0,
-              results: [{}],
-            }),
-        } as Response),
-      );
+      const fakeData = {
+        response_code: 0,
+        results: [{}],
+      };
 
-      global.fetch = mockFetch;
+      if (typeof global.fetch === 'function') {
+        jest.spyOn(global, 'fetch').mockImplementation(setupFetchStub(fakeData));
+      } else {
+        global.fetch = jest.fn().mockImplementation(setupFetchStub(fakeData));
+      }
+
       await getQuestions({
         difficulty: 'easy',
         type: 'multiple',
       });
-      expect(mockFetch).toHaveBeenCalledWith('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
-      global.fetch = globalFetch;
+      expect(global.fetch).toHaveBeenCalledWith('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
+
+      (global.fetch as jest.MockedFunction<typeof global.fetch>).mockClear();
     });
   });
 });
